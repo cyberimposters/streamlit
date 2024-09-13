bed6aeca1a9fe56f35793365b35ca21146c04da2
Jordyn Short <98502751+cyberimposters@users.noreply.github.com>
2024-09-12 20:02:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ queries = {
     "elasticsearch": 100,  # Up to startIndex 80
     "kibana": 100,         # Up to startIndex 60
     "logstash": 100,       # Up to startIndex 20
-    "Oracle GraalVM for JDK": 20  # Up to startIndex 20, note the phrase queryType
+    "Oracle GraalVM for JDK": 100  # Up to startIndex 20, note the phrase queryType
 }
 
 # Generate URLs based on queries and pagination
